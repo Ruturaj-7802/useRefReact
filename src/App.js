bcb0cd33ad5e7d3f0840fbf5995bd32a2929c98a
Ruturaj <ruturaj.7802@gmail.com>
2022-12-10 19:32:11 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Assgn from './components/Assgn'
+
+function App() {
+  return (
+    <div className='container'>
+      <Assgn/>
+    </div>
+  );
+}
+
+export default App;
